08a67e648454a447a0814c13a16f427b739278d9
mateobaraldi <mateobaraldi5@gmail.com>
2026-09-24 19:07:23 -0300

    Delete demo/demo directory

--- demo/demo/src/main/java/com/example/demo/exception/ErrorResponse.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.exception;
-
-import java.time.LocalDateTime;
-
-public record ErrorResponse(
-        int status,
-        String message,
-        LocalDateTime timestamp
-) {}
